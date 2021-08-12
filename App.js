15c197fd07ffe1aa9fe8a32cfd2b7e9b464f5971
Adam Juwig <adamjuwig@gmail.com>
2021-08-12 13:51:51 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/free-brands-svg-icons';
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>
+    <Router>
+      
+        <Navbar />
+        <Header />
+
+        <Route exact path="/" component={About} />
+
+        <Switch>
+          
+          <Route path="/Projects" component={Projects} />
+          <Route path="/Contact" component={Contact} />
+        </Switch>
+        
+       <Footer />
+    </Router>
+   
+    </>
+  );
+}
+
+export default App;
